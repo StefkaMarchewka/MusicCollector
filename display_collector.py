fdd83684a37d9c6b3726610fc1647934ea395629
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2018-12-07 11:58:33 +0100

    after evaluation version

--- display_collector.py
+++ display_collector.py
@@ -1,35 +1,63 @@
 #display menu
 from music_report import* #import file
 
-#utworyc zmienną w mainie, która bedzie zawierac tabele (all_records)
 
 def menu(): #pytamy co chce zrobic uzytkownik 
-    while True:
-        print(" 1. View full collection\n 2. Search by artist\n 3. Search by album\n 4. Search by year\n 5. Search by genre\n 6. Search by lenght\n 7. Print raport")
-        answer = input("Type the number:") # trzeba aby input nie był case sensitiv, casefold?
+    os.system("clear")
+    user_not_pressed_defined_number = True
+    while user_not_pressed_defined_number:
+        
+        print(""" 
+        MENU    
+    1. View full collection
+    2. Search by artist 
+    3. Search by album 
+    4. Search by year 
+    5. Search by genre
+    6. Search by lenght 
+    7. Print report
+    8. Exit
+    """)
+        answer = input("Choose the option from menu (number):") 
         if answer == "1":
-            draw_table(all_records)
+            os.system("clear")
+            print(draw_table(all_records))
         elif answer == "2":
-            choice = input("type artist name:")
+            os.system("clear")
+            choice = input("Type artist name:")
             print(search_all(all_records, choice))
         elif answer == "3":
-            choice = input("type album name:")
+            os.system("clear")
+            choice = input("Type album name:")
             print(search_all(all_records, choice))
         elif answer == "4":
-            choice = input("type year:")
-            print(search_all(all_records, choice))
+            os.system("clear")
+            print(time_range(all_records))
         elif answer == "5":
-            choice = input("type genre name:")
+            os.system("clear")
+            choice = input("Type genre name:")
             print(search_all(all_records, choice))
-        elif answer == "6": #nie zrobiona
-            choice = input("type lenght:") 
-            print(album_time(all_records))
+        elif answer == "6": 
+            os.system("clear")
+            choice = input("choose longest(L) or shortest(S) album:") 
+            if choice == "L":
+                print("The longest album is:", longest_album(all_records))
+            elif choice == "S":
+                print("The shortest album is:", shortest_album(all_records))
         elif answer == "7":
-            album_sum(all_records)
-            album_genre(all_records)
+            os.system("clear")
+            print("Collection report:")
+            print("Collection contains ", album_sum(all_records), "albums")
+            print("Albums by genre:", album_genre(all_records))
             print("The youngest album is: ", youngest_album(all_records))
             print("The oldest album is: ", oldest_album(all_records))
-            album_time(all_records)
+            print("The shortest album is:", shortest_album(all_records))
+            print("The longest album is:", longest_album(all_records))
+        elif answer == "8":
+            user_not_pressed_defined_number = False
+        elif not answer.isnumeric(): 
+            os.system("clear")
+            print("Wrong input, choose a number")
        
         
 
