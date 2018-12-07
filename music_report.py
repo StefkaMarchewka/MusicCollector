e4fcf747e9bd415f4e8109f8045aaee76599a9d6
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2018-12-07 11:58:56 +0100

    after evaluation version

--- music_report.py
+++ music_report.py
@@ -1,6 +1,8 @@
 #music reports
 from prettytable import PrettyTable
 from collections import Counter
+import sys
+import os
 
 
 def import_file(filename):
@@ -12,29 +14,27 @@ def import_file(filename):
             full_content.append(new_line)
         return full_content
 
-all_records = import_file("/home/stefania/Desktop/current_week/library.txt")  #cały kontent'''
+
+all_records = import_file("library.txt")  #cały kontent'''
                            #cała scieżka dostepu musi byc
+
 
 def draw_table(all_records):  #działa!
     table = PrettyTable()
-    table.field_names = ["artist name","album name","release year","genre","length"]
+    table.field_names = ["Artist name","Album name","Release year","Genre","Length"]
     for record in all_records: 
         table.add_row(record)
-    print(table)
+    return table
 
-printed_table = draw_table
+# printed_table = draw_table  '''to był potrzebne przy importowaniu zmiennych
 
-# def search_by_sth(all_records, answer):  #zwraca tylko pieprwszy znaleziony rekord
-#     for record in all_records:
-#         if answer in record:
-#             return record
 
 def search_all(all_records, answer): #dziala, wyszukuje wszystkie rekordy dla danego klucza
     search_key = []
     for record in all_records:
         if answer in record:
             search_key.append(record)
-    return search_key #trzeba zmienić printy na returny
+    return draw_table(search_key) #trzeba zmienić printy na returny
                       
 
 def youngest_album(all_records): #działa!!! 
@@ -42,10 +42,10 @@ def youngest_album(all_records): #działa!!!
     for record in all_records:
         years.append(int(record[2])) #żeby wybrac min i max elementy listy musza byc int
     youngest = max(years)
-    for record in all_records:  #iterowac można po itertorach! nie moze iterowac po wybranych elementach listy, moze po całych listach
+    for record in all_records:  #iterowac można po iteratorach! nie moze iterowac po wybranych elementach listy, moze po całych listach
         if youngest == int(record[2]): 
             return record
- 
+
  
 def oldest_album(all_records): 
     years = []
@@ -57,7 +57,24 @@ def oldest_album(all_records):
             return record  
         
 
-def album_time(all_records): #działa!
+def shortest_album(all_records): #działa!
+    times = []  #zrobic jeden słownik a nie dwie listy
+    duration = []
+    for record in all_records:
+        times.append(record[4])
+    for item in times:
+        (m, s) = item.split(":")
+        result = int(m) * 60 + int(s)
+        duration.append(result)
+        shortest = min(duration)
+        short_ind = (duration.index(shortest))  #indeks na liscie najkrótszego albumu
+    min_minutes = times[short_ind]
+    for record in all_records:  
+        if min_minutes == record[4]: 
+           return record
+
+
+def longest_album(all_records): #działa!
     times = []  #zrobic jeden słownik a nie dwie listy
     duration = []
     for record in all_records:
@@ -67,26 +84,37 @@ def album_time(all_records): #działa!
         result = int(m) * 60 + int(s)
         duration.append(result)
         longest = max(duration)
-        shortest = min(duration)
         long_ind = (duration.index(longest)) #indeks na liscie najdłuższego albumu
-        short_ind = (duration.index(shortest))  #indeks na liscie najkrótszego albumu
     max_minutes = times[long_ind] #pokazuje najdłuzszy czas
-    min_minutes = times[short_ind]
     for record in all_records:  
         if max_minutes == record[4]: 
-            print("the longest album:", record)
-        if min_minutes == record[4]: 
-            print("the shortest album:", record)
-
+            return record
+        
 
 def album_sum(all_records):
-    print("Collection contains ", len(all_records), "albums")
+    return len(all_records)
+
 
 def album_genre(all_records):
     genres = []
     for record in all_records:
         genres.append(record[3])
-    print(Counter(genres))
+    return Counter(genres)
+
+def time_range(all_records):
+    input_range = []
+    first_year = int(input("choose start year:"))
+    second_year = int(input("choose end year:"))
+    input_range.append(first_year)
+    input_range.append(second_year)
+    result = []
+    for record in all_records:
+        if int(record[2]) in range(input_range[0], input_range[1]+1):
+            result.append(record)
+    return draw_table(result)
+
+
+
 
 
 
